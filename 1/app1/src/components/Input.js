eb84d9a4ef2570c93d67d6fa78713ae4336a33fb
Vitaliy <mv703021@gmail.com>
2021-01-27 22:00:22 +0200

    2 methods combined2

--- 1/app1/src/components/Input.js
+++ 1/app1/src/components/Input.js
@@ -2,10 +2,8 @@ import React, {useState} from "react";
 import '../App.css';
 
 export default function Input({calculationHandler}) {
-    //todo позбавитися рефа і зробити контрольований інпут
 
     const [inputValue, setInputValue] = useState(0);
-    console.log(inputValue);
     return (
         <div className='submit-box'>
             <input type="number" name='inp' onChange={(e) => setInputValue(+(e.target.value))} value={inputValue}/>
